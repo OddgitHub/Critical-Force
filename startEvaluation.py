e05d5a898e7333987cf2e499ab5ec3ae5b560237
OddgitHub <philippbulling@gmail.com>
2022-12-15 08:24:14 +0100

    Added files from old console application

---
+++ startEvaluation.py
@@ -0,0 +1,73 @@
+from workout_config.workouts import WorkoutHandler
+import os, csv, re
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import filtfilt
+
+print('\n---------------------------')
+print('Available workout results: ')
+print('---------------------------')
+
+# Print available workout results
+allResults = os.listdir('results')
+for i, file in enumerate(allResults):
+    if file.endswith('.csv'):
+        print(str(i) + ' - ' + file)
+
+selectedFile = 'results\\' + allResults[int(input('\nPlease select a workout: '))]
+
+# Load results file
+with open(selectedFile, mode='r') as csvFile:
+    csvReader = csv.DictReader(csvFile, delimiter=';')
+    row = next(csvReader)
+
+emgSignal = np.load(row['Data'])
+workoutID = int(row['Workout ID'])
+fsSensor = int(row['Sample Rate'])
+
+# Load corresponding workout
+workoutCfgPath = ('./workout_config/workouts.csv')
+workoutHandler = WorkoutHandler(workoutCfgPath)
+lookupTable  = workoutHandler.getLookupTables(workoutID)[0]
+setDur, numSets, setPause, startPause = workoutHandler.getTotalSetDuration(workoutID)
+
+# Filter EMG signal
+#filterOrd = 20
+#emgSignal = filtfilt(np.divide(np.ones(filterOrd), filterOrd), 1, emgSignal)
+
+emgSignalSetWise = np.zeros((setDur * fsSensor, numSets))
+for i in range(numSets):
+    emgSignalSetWise[:,i] = emgSignal[fsSensor * (startPause + i*setDur):fsSensor * (startPause + i*setDur + setDur)]
+
+    ###################################################################################
+    # TODO: Mittelwert über aktive Zeit / Pause für jedes Set berechnen und auswerten?
+    ###################################################################################
+
+#====================
+# Plot total workout
+plt.figure(num=None, figsize=(15, 12), dpi=80, facecolor='w', edgecolor='k')
+sp1 = plt.subplot(3,1,1)
+plt.plot(np.divide(list(range(len(emgSignal))), fsSensor), emgSignal)
+plt.grid()
+plt.title('EMG signal')
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude [V]')
+
+plt.subplot(3,1,2, sharex=sp1)
+plt.plot(list(range(len(lookupTable))), lookupTable)
+plt.grid()
+plt.title('Timer')
+plt.xlabel('Time [s]')
+plt.ylabel('Pause | Active')
+
+#===========
+# Plot sets
+plt.subplot(3,1,3)
+hPlot = []
+for i in range(numSets):
+    [curPlot] = plt.plot(np.divide(list(range(emgSignalSetWise.shape[0])), fsSensor), emgSignalSetWise[:,i], label='Set No. ' + str(i+1))
+    hPlot.append(curPlot)
+
+plt.legend(hPlot)
+plt.grid()
+plt.show()
---
+++ workout_config/workouts.py
@@ -0,0 +1,83 @@
+import csv
+import numpy as np
+
+class WorkoutHandler:
+
+	def __init__(self, csvFile):
+		self.allWorkouts = []
+		self.startPause = 5		# This pause is automatically added at the beginning of the workout
+		self.cntInDur = 3		# This is the count-in before a new activity starts
+
+		with open(csvFile, mode='r') as csvFile:
+			csvReader = csv.DictReader(csvFile, delimiter=';')
+			for row in csvReader:
+				self.allWorkouts.append(row)
+
+	def getAllWorkouts(self):
+		return self.allWorkouts
+
+	def getLookupTables(self, workoutID):
+		selectedWorkout = self.allWorkouts[workoutID]
+
+		startPause  = self.startPause
+		numSets     = int(selectedWorkout['Sets'])
+		setPause    = int(selectedWorkout['Set Pause'])
+		repsPerSet  = int(selectedWorkout['Repetitions'])
+		repActive   = int(selectedWorkout['Repetition Active'])
+		repPause    = int(selectedWorkout['Repetition Pause'])
+		cntInDur	= self.cntInDur
+
+		assert(cntInDur <= startPause)
+		assert(cntInDur <= setPause)
+		#assert(cntInDur <= repPause)
+
+		# Set duration without set pause
+		setDur = repsPerSet * (repActive + repPause) - repPause
+
+		# Total duration
+		totalDur = numSets * (setDur + setPause) + startPause
+
+		#======================================
+		# Create look-up table for the workout 
+		lookupTable = np.zeros(totalDur, dtype='int16')
+		lookupTable[0:startPause] = 0
+		repPattern = np.zeros(setDur, dtype='int16')
+
+		countdown = np.zeros(totalDur, dtype='int16')
+		countdown[0:startPause] = list(range(startPause,0,-1))
+		cntPattern = np.zeros(setDur + setPause, dtype='int16')
+		cntPattern[setDur:] = list(range(setPause,0,-1))
+
+		for i in range(repsPerSet):
+			repPattern[i*(repActive+repPause):(i*(repActive+repPause)+repActive)] = 1
+			cntPattern[i*(repActive+repPause):(i*(repActive+repPause)+repActive)] = list(range(repActive,0,-1))
+			if i < repsPerSet - 1:
+				cntPattern[(i*(repActive+repPause)+repActive):(i*(repActive+repPause)+repActive+repPause)] = list(range(repPause,0,-1))
+
+		for i in range(numSets):
+			lookupTable[startPause + i*(setDur + setPause):startPause + i*(setDur + setPause) + setDur] = repPattern
+			countdown[startPause + i*(setDur + setPause):startPause + i*(setDur + setPause) + setDur + setPause] = cntPattern
+
+		#==================================
+		# Create look-up table for countin
+		# TODO: This can be replaced by the countdown and is no longer necessary
+		countin = np.roll(lookupTable, -cntInDur)
+		countin = np.maximum(countin - lookupTable, 0)
+
+		return lookupTable, countdown, countin
+
+	def getTotalSetDuration(self, workoutID):
+		selectedWorkout = self.allWorkouts[workoutID]
+
+		startPause  = self.startPause
+		numSets     = int(selectedWorkout['Sets'])
+		setPause    = int(selectedWorkout['Set Pause'])
+		repsPerSet  = int(selectedWorkout['Repetitions'])
+		repActive   = int(selectedWorkout['Repetition Active'])
+		repPause    = int(selectedWorkout['Repetition Pause'])
+
+		# Set duration without set pause
+		setDur = repsPerSet * (repActive + repPause) - repPause + setPause
+
+		return setDur, numSets, setPause, startPause
+
