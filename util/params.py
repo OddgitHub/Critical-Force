4c751d7483253416d07ec1f7dd7fdef0bfc5fb98
OddgitHub <philippbulling@gmail.com>
2022-12-24 14:26:40 +0100

    Replaced Icon

--- util/params.py
+++ util/params.py
@@ -1,9 +1,10 @@
 from enum import Enum
+import os
 
 class Params(Enum):
     fsMeasurement = 10       # [Hz]
     workoutCfgPath = ('./workouts/workouts.csv')
     fileClickHi = ('./raw/clickhi.wav')
     fileClickLo = ('./raw/clicklo.wav')
-    appIcon = ('./raw/icon.png')
+    appIcon = ('./raw/icon.ico')
     appName = 'Climbing Trainer'
