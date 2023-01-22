e02ca04e6546ba7f86f8d747d9a36bfde58cd772
OddgitHub <philippbulling@gmail.com>
2023-01-22 19:31:43 +0100

    Updated about window

--- ui/About.py
+++ ui/About.py
@@ -9,14 +9,22 @@ class AboutWindow(QWidget):
             "This software tool can be used to capture climbing specific finger strength measures, such as critical force. "
             "The software is free. It requires a fingerboard, attached to a load-cell. "
             "The load-cell has to be connected to a NAU7802 amplifier. The amplifier has to be connected to a MCP2221A "
-            "breakout board via I2C.\n\n"
-            "The graphical user interface was built with PySide6 and PyQtGraph and therefore this software licensed under GPLv2.\n\n"
-            "If you encounter any issues, please reach out to philippbulling@gmail.com.\n")
+            "breakout board via I2C.")
         aboutLabel.setWordWrap(True)
+
+        linkLabel = QLabel("<a href=\"https://philaudio.wordpress.com/projects/climbing\">More information can be found here.</a>" )
+        linkLabel.setOpenExternalLinks(True)
+
+        licenseLabel = QLabel("The graphical user interface was built with PySide6 and PyQtGraph and therefore this software licensed under GPLv2.\n\n"
+            "If you encounter any issues, please reach out to philippbulling@gmail.com.")
+        licenseLabel.setWordWrap(True)
+
         nameLabel = QLabel("Dr.-Ing. Philipp Bulling, 2023")
         versionLabel = QLabel("Software Version: " + Params.version.value)
 
         layout.addWidget(aboutLabel)
+        layout.addWidget(linkLabel)
+        layout.addWidget(licenseLabel)
         layout.addWidget(nameLabel)
         layout.addWidget(versionLabel)
         self.setLayout(layout)
