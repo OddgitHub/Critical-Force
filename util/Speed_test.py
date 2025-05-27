acc55d6717af1521590b66300680ad694220e5b8
threedegreesofseperation <kirowt00@hs-esslingen.de>
2025-05-27 22:01:00 +0200

    speed test

---
+++ util/Speed_test.py
@@ -0,0 +1,3 @@
+"1.Versuch: glätten und dann mithilfe von Steigung: nicht geklappt weil entweder zu arg geglättet oder steigung nicht hoch genug"
+"2.Verscuh: nicht geglättet und mithilfe von Steigung: nicht geklappt weil steigung zu hoch ist davor"
+"3.Versuch nicht geglättet und Steigung mit vielen aufeinanderfolgenden Werten"
